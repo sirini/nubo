ef119c71baedaa1988f99294601df8f6735cadc1
sirini <sirini@gmail.com>
2025-07-13 00:08:13 +0900

    Nubo 첫페이지 시안 잡기 반영 (좌측 사이드바, 우측 컨텐츠)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,33 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
+import tailwindcss from "@tailwindcss/vite"
 
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/css/tailwind.css"],
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts'
-  ]
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "shadcn-nuxt",
+  ],
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
+  app: {
+    head: {
+      titleTemplate: "Nubohub | The next evolution of tsboard",
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Outfit:wght@100;400;700;900&display=swap",
+        },
+      ],
+    },
+  },
 })
