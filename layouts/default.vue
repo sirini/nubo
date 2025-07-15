aa9a43cf77d3daf16ff05c2988ead0b15be14aaf
sirini <sirini@gmail.com>
2025-07-15 22:19:44 +0900

    Nubo 첫페이지 시안 재조정 (사이드바 제거)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
-    <slot />
-</template>
+  <div class="min-h-screen">
+    <div class="flex flex-col bg-background text-foreground">
+      <topmenu-nav-menus />
+    </div>
 
-<script setup lang="ts"></script>
+    <main class="container mx-auto flex-1 px-4 py-6 min-h-[90vh]">
+      <slot />
+    </main>
+
+    <footer class="py-4 text-center text-sm text-muted-foreground font-mono">
+      Powered by <a href="https://nubohub.org" target="_blank">Nubo</a>
+    </footer>
+  </div>
+</template>
