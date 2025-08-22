647ae87ff9529bc2bc834c6d11650eaffb545591
sirini <sirini@gmail.com>
2025-08-22 13:22:35 +0900

    첫 페이지에서 카드 형태로 출력할 때 커버 이미지가 있을 경우 상단 여백 없이 꽉 차게 보이도록 개선, 글작성 일자는 xl 스크린 크기가 아니면 숨기도록 변경, 글내용 미리보기는 3줄, 제목은 1줄만 고정해서 나오도록 변경

--- components/main/PostCard.vue
+++ components/main/PostCard.vue
@@ -1,14 +1,13 @@
 <template>
-  <Card class="relative overflow-hidden rounded-xl shadow-lg">
-    <div class="w-full aspect-[1/1] overflow-hidden" v-if="post.cover">
-      <img
-        :src="post.cover"
-        alt="cover image"
-        class="absolute w-full aspect-[1/1] inset-0 object-cover transition-transform hover:scale-105"
-      />
-    </div>
-    <CardHeader>
-      <CardTitle class="leading-6 mb-2">{{ post.title }}</CardTitle>
+  <Card class="overflow-hidden rounded-lg shadow-lg pt-0">
+    <img
+      v-if="post.cover"
+      :src="post.cover"
+      alt="cover image"
+      class="w-full aspect-[1/1] object-cover transition-transform hover:scale-105"
+    />
+    <CardHeader class="px-3">
+      <CardTitle class="line-clamp-1 mb-2">{{ post.title }}</CardTitle>
       <CardDescription class="inline-flex items-center">
         <Heart :class="post.liked ? 'text-red-200 fill-current' : ''" class="w-3 h-3 mr-2" />
         {{ post.like }}
@@ -17,10 +16,10 @@
         <Eye class="w-3 h-3 ml-4 mr-2" />
         {{ showReadableNumber(post.hit) }}
         <div class="flex-1"></div>
-        {{ showDateOnly(post.submitted) }}
+        <span class="hidden xl:block">{{ showDateOnly(post.submitted) }}</span>
       </CardDescription>
     </CardHeader>
-    <CardContent class="text-sm line-clamp-5 leading-5">{{
+    <CardContent class="text-sm line-clamp-3 leading-5 px-3">{{
       stripHtmlTags(post.content)
     }}</CardContent>
   </Card>
