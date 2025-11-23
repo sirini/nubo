b11e43783f765c4725ce523469162cbb47581a52
sirini <sirini@gmail.com>
2025-11-23 16:37:54 +0900

    프로필 수정에서 프로필 이미지 보이도록 개선, 메뉴 버튼을 상단 좌측으로 이동, 소셜 로그인에 네이버, 카카오 버튼 추가

--- app/components/top/Menu.vue
+++ app/components/top/Menu.vue
@@ -1,21 +1,19 @@
 <template>
   <div class="flex items-center">
-    <CommonVTooltip content="첫 화면으로 이동합니다">
-      <NuxtLink to="/" class="font-bold text-lg mr-3">
-        <span>{{ config.public.title }}</span>
-      </NuxtLink>
-    </CommonVTooltip>
-
     <DropdownMenu>
       <DropdownMenuTrigger as-child>
         <MenuIcon class="w-6 h-6 cursor-pointer" />
       </DropdownMenuTrigger>
       <DropdownMenuContent class="w-48" align="start">
-        <DropdownMenuLabel class="text-gray-500">내 계정</DropdownMenuLabel>
+        <DropdownMenuLabel class="text-gray-500 text-xs">내 계정</DropdownMenuLabel>
         <DropdownMenuGroup v-if="auth.isLoggedIn">
           <DropdownMenuItem as-child class="w-full">
             <NuxtLink to="/auth/profile" class="inline-flex gap-3 items-center">
-              <UserPenIcon class="w-4 h-4" /> 프로필 수정
+              <Avatar>
+                <AvatarImage :src="auth.user.profile" alt="Profile image" />
+                <AvatarFallback>{{ auth.user.name.charAt(0) }}</AvatarFallback>
+              </Avatar>
+              프로필 수정
             </NuxtLink>
           </DropdownMenuItem>
           <DropdownMenuItem as-child class="w-full">
@@ -34,7 +32,7 @@
 
         <DropdownMenuSeparator />
 
-        <DropdownMenuLabel class="text-gray-500">메뉴</DropdownMenuLabel>
+        <DropdownMenuLabel class="text-gray-500 text-xs">메뉴</DropdownMenuLabel>
         <DropdownMenuGroup v-if="data">
           <DropdownMenuSub v-for="(menu, index) in data.result" :key="index">
             <DropdownMenuSubTrigger
@@ -59,6 +57,12 @@
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
+
+    <CommonVTooltip content="첫 화면으로 이동합니다">
+      <NuxtLink to="/" class="font-bold text-lg ml-3">
+        <span>{{ config.public.title }}</span>
+      </NuxtLink>
+    </CommonVTooltip>
   </div>
 </template>
 
@@ -72,14 +76,8 @@ import {
 
 import { useRuntimeConfig } from "#app"
 import { useHomeStore } from "#imports"
-import {
-  FolderOpenIcon,
-  FoldersIcon,
-  LogInIcon,
-  LogOutIcon,
-  MenuIcon,
-  UserPenIcon,
-} from "lucide-vue-next"
+import { FolderOpenIcon, FoldersIcon, LogInIcon, LogOutIcon, MenuIcon } from "lucide-vue-next"
+import AvatarImage from "../ui/avatar/AvatarImage.vue"
 
 const { fetchHomeMenus } = useHome()
 const config = useRuntimeConfig()
--- app/components/ui/avatar/AvatarFallback.vue
+++ app/components/ui/avatar/AvatarFallback.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { reactiveOmit } from "@vueuse/core"
-import { AvatarFallback, type AvatarFallbackProps } from "reka-ui"
+import type { AvatarFallbackProps } from "reka-ui"
 import type { HTMLAttributes } from "vue"
-import { cn } from "~/lib/utils"
+import { reactiveOmit } from "@vueuse/core"
+import { AvatarFallback } from "reka-ui"
+import { cn } from "@/lib/utils"
 
 const props = defineProps<AvatarFallbackProps & { class?: HTMLAttributes["class"] }>()
 
