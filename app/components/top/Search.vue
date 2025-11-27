52b9c9c7b24f6a397684e793976c7b7c2f58adce
sirini <sirini@gmail.com>
2025-11-27 21:19:02 +0900

    검색 버튼은 2글자 이상 입력 시 활성화, 툴팁 적용

--- app/components/top/Search.vue
+++ app/components/top/Search.vue
@@ -1,8 +1,20 @@
 <template>
   <form @submit="onSubmit" class="flex gap-2">
     <CommonVSelect v-model="home.option" :options="options" placeholder="검색 조건 선택" />
-    <Input v-model="home.keyword" type="text" placeholder="검색어를 입력하세요" />
-    <Button type="submit" variant="outline" class="text-foreground">찾기</Button>
+
+    <CommonVTooltip content="검색어는 2글자 이상 입력해주세요">
+      <Input v-model="home.keyword" type="text" placeholder="검색어를 입력하세요"
+    /></CommonVTooltip>
+
+    <CommonVTooltip content="선택하신 검색 옵션과 키워드로 게시글을 찾아봅니다">
+      <Button
+        type="submit"
+        variant="outline"
+        class="text-foreground"
+        :disabled="home.keyword.length < 2"
+        >찾기</Button
+      ></CommonVTooltip
+    >
   </form>
 </template>
 
