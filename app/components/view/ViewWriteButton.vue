c84912860a2d0c99bd5b23c44ff761d53edbd295
sirini <sirini@gmail.com>
2025-11-30 17:08:24 +0900

    글작성 버튼을 별도 컴포넌트로 분리

---
+++ app/components/view/ViewWriteButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <CommonVTooltip content="새로운 글을 작성합니다">
+    <Button
+      as-child
+      variant="default"
+      class="text-foreground"
+      :class="{ 'opacity-50 pointer-events-none': !auth.isLoggedIn }"
+    >
+      <NuxtLink :to="`/board/${boardId}/write`" :aria-disabled="!auth.isLoggedIn"
+        >새글작성</NuxtLink
+      >
+    </Button>
+  </CommonVTooltip>
+</template>
+
+<script setup lang="ts">
+const auth = useAuthStore()
+
+defineProps<{
+  boardId: string
+}>()
+</script>
--- app/pages/board/[id]/[postUid].vue
+++ app/pages/board/[id]/[postUid].vue
@@ -102,18 +102,7 @@
             </Button>
           </CommonVTooltip>
 
-          <CommonVTooltip content="새로운 글을 작성합니다">
-            <Button
-              as-child
-              variant="default"
-              class="text-foreground"
-              :class="{ 'opacity-50 pointer-events-none': !auth.isLoggedIn }"
-            >
-              <NuxtLink :to="`/board/${boardId}/write`" :aria-disabled="!auth.isLoggedIn"
-                >새글작성</NuxtLink
-              >
-            </Button>
-          </CommonVTooltip>
+          <ViewWriteButton :board-id="boardId" />
         </div>
       </div>
     </div>
