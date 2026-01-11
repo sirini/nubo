11757f684cb2d9bc5d61a909917361e4ca1a8102
sirini <sirini@gmail.com>
2026-01-11 13:36:37 +0900

    게시판 가로폭 크기 지정 다시 원복

--- app/skins/board/nubo-basic-board/BlogView.vue
+++ app/skins/board/nubo-basic-board/BlogView.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="relative min-h-screen bg-background mx-auto py-4"
-    :class="`max-w-[${config.width}px]`"
+    :style="`max-width: ${config.width}px`"
   >
     <div
       class="fixed top-0 left-0 w-full h-0.5 bg-primary z-50 origin-left"
--- app/skins/board/nubo-basic-board/DefaultWrite.vue
+++ app/skins/board/nubo-basic-board/DefaultWrite.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container mx-auto py-4">
-    <div class="mx-auto" :class="`max-w-[${config.width}px]`">
+    <div class="mx-auto" :style="`max-width: ${config.width}px`">
       <Card>
         <CardHeader>
           <CardTitle>새글쓰기</CardTitle>
