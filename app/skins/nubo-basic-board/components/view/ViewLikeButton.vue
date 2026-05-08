c837b4fcf454980fb5386b2e0ef98df1e5408ebb
sirini <sirini@gmail.com>
2026-05-08 23:06:24 +0900

    게시글에 대한 좋아요 버튼도 로그인 해야만 활성화 되도록 수정

--- app/skins/nubo-basic-board/components/view/ViewLikeButton.vue
+++ app/skins/nubo-basic-board/components/view/ViewLikeButton.vue
@@ -1,12 +1,12 @@
 <template>
   <CommonVTooltip content="이 게시글에 좋아요를 취소합니다" v-if="view.post.liked">
-    <Button variant="outline" class="cursor-pointer" size="lg" @click="likePost(false)">
+    <Button variant="outline" class="cursor-pointer" size="lg" @click="likePost(false)" :disabled="!isLoggedIn">
       <HeartIcon class="fill-red-300 text-red-300" />
     </Button>
   </CommonVTooltip>
 
   <CommonVTooltip content="이 게시글에 좋아요를 남깁니다" v-else>
-    <Button variant="outline" class="cursor-pointer" size="lg" @click="likePost(true)">
+    <Button variant="outline" class="cursor-pointer" size="lg" @click="likePost(true)" :disabled="!isLoggedIn">
       <HeartIcon class="text-red-300" />
     </Button>
   </CommonVTooltip>
@@ -16,5 +16,5 @@
 import { HeartIcon } from "lucide-vue-next"
 import { useNuboViewContext } from "~/providers/contexts/view"
 
-const { view, likePost } = useNuboViewContext()
+const { view, isLoggedIn, likePost } = useNuboViewContext()
 </script>
