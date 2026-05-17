f156f4737f1f97334157e0faeaae191e081237fb
sirini <sirini@gmail.com>
2026-05-17 17:59:45 +0900

    (v1.1.1) 홈화면에서 랜딩 페이지 및 히어로 배너 섹션 삭제

--- app/skins/nubo-basic-home/Home.vue
+++ app/skins/nubo-basic-home/Home.vue
@@ -1,16 +1,7 @@
 <template>
-  <Landing v-if="!isSearching && isLanding" />
-  <LatestPosts v-else />
+  <LatestPosts />
 </template>
 
 <script setup lang="ts">
-import { useNuboHomeContext } from "~/providers/contexts/home"
-import Landing from "./Landing.vue"
 import LatestPosts from "./LatestPosts.vue"
-
-const route = useRoute()
-const isSearching = computed(
-  () => route.params.keyword !== undefined && route.params.keyword.length > 1,
-)
-const { isLanding } = useNuboHomeContext()
 </script>
